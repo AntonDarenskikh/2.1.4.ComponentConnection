356c1167aa34f221d3f88be9b2d166ed95b0b5b5
Anton Darenskikh <darenskikh.spb@yandex.ru>
2024-03-12 10:20:31 +0300

    Ñadd variety

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -5,8 +5,12 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Needle7 {
-    @Autowired
+
     private Deth8 death;
+
+    public Needle7(Deth8 death) {
+        this.death = death;
+    }
 
     @Override
     public String toString() {
